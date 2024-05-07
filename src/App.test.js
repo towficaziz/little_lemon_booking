5f1f686ca507ca5d7cc9f77ae63919383cce1fc7
towficaziz <tapolly01@gmail.com>
2024-05-07 14:13:37 +0200

    adding test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
+import BookingForm from "./components/BookingForm";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the BookingForm heading', () => {
+    const availableTimes = { availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"] };
+    render(<BookingForm availableTimes={availableTimes} />);
+    const headingElement = screen.getByText("Confirm Reservation");
+    expect(headingElement).toBeInTheDocument();
+
 });
+
+test('Reserve Table', () => {
+  render(<App />);
+  const reserveButton = screen.getByText("Reserve Table");
+  fireEvent.click(reserveButton);
+});
+
