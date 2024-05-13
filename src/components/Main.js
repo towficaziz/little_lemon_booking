a5fd12eba1f3fd7c11588d1e42ce089050685ae2
towficaziz <tapolly01@gmail.com>
2024-05-14 00:15:02 +0200

    code changing of Choose a Time API

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,29 +8,39 @@ const Main = () => {
   // const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
 
   //Chrome was blocking running the script on the index page so I added it here. "https://chromestatus.com/feature/5629709824032768"
-  const seededRandom = function (seed) {
-    var m = 2 ** 35 - 31;
-    var a = 185852;
-    var s = seed % m;
-    return function () {
-      return (s = (s * a) % m) / m;
-    };
-  };
+  
+  // const seededRandom = function (seed) {
+  //   var m = 2 ** 35 - 31;
+  //   var a = 185852;
+  //   var s = seed % m;
+  //   return function () {
+  //     return (s = (s * a) % m) / m;
+  //   };
+  // };
 
-  const fetchAPI = function (date) {
-    let result = [];
-    let random = seededRandom(date.getDate());
+  // const fetchAPI = function (date) {
+  //   let result = [];
+  //   let random = seededRandom(date.getDate());
 
-    for (let i = 17; i <= 23; i++) {
-      if (random() < 0.5) {
-        result.push(i + ":00");
-      }
-      if (random() < 0.5) {
-        result.push(i + ":30");
-      }
+  //   for (let i = 17; i <= 23; i++) {
+  //     if (random() < 0.5) {
+  //       result.push(i + ":00");
+  //     }
+  //     if (random() < 0.5) {
+  //       result.push(i + ":30");
+  //     }
+  //   }
+  //   return result;
+  // };
+
+  function fetchAPI() {
+    const timeSlots = [];
+    for (let hour = 16; hour <= 23; hour++) {
+      timeSlots.push(`${hour}:00`);
+      timeSlots.push(`${hour}:30`);
     }
-    return result;
-  };
+    return timeSlots;
+  }
   const submitAPI = function (formData) {
     return true;
   };
